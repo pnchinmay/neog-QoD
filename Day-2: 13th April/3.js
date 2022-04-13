1039c24a920284c18ab8a27b31a25dc4bac607c3
pnchinmay <pnchinmay9@gmail.com>
2022-04-13 19:33:57 +0530

    feat: add soln for Q3

--- Day-2: 13th April/3.js
+++ Day-2: 13th April/3.js
@@ -1,17 +1,20 @@
-const upper = (ch) => {
-  if(ch.charCodeAt)
-}
 const toSentenceCase = (str1) => {
   let str2 = ""
   for(let i=0; i<str1.length; i++){
     if(i==0)
-      str2 += str1[0].toUpperCase();
-    else if(str1[i]==" ")
       str2 += str1[i].toUpperCase();
-    else
-      str2 += str1[i]
+    else if(str1[i]==" "){
+      str2 += " "
+      str2 += str1[i+1].toUpperCase();
+    }
+    else{
+      if(str1[i-1]==" ");
+      else
+        str2 += str1[i]
+    }
   }
   return str2;
 }
-a = "We are neog";
+
+let a = "we are neog"
 console.log(toSentenceCase(a))
